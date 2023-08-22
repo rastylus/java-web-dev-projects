581ae12542660621f5db62d41695c87562e33df4
rastylus <ryan.p.riggs@gmail.com>
2023-08-21 20:00:12 -0500

    added negative number check

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,11 +1,18 @@
 package org.launchcode;
 import java.util.Scanner;
 
+
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
         Double radius = input.nextDouble();
+        if (radius <= 0) {
+            System.out.println("Error");
+            System.exit(0);
+        } else if(Double.isNaN(radius)) {
+            System.out.println("Error");
+            System.exit(0);
         Double circ = Circle.getArea(radius);
 //        Double circ = radius * radius * 3.14;
         System.out.println("The area of a circle of " + radius + " is: " + circ);
