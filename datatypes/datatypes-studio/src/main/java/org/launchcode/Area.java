ddf56c84ab996b2e153d12cdc70f40d24d63735a
rastylus <ryan.p.riggs@gmail.com>
2023-08-21 19:41:00 -0500

    area of a circle

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,7 +6,8 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
         Double radius = input.nextDouble();
-        Double circ = radius*radius*3.14;
+        Double circ = Circle.getArea(radius);
+//        Double circ = radius*radius*3.14;
         System.out.println("The area of a circle of " + radius + " is: " + circ);
     }
 }
