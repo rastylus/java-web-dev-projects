5be6044f938a5791d8873b76c90e03d510441e1c
rastylus <ryan.p.riggs@gmail.com>
2023-08-28 02:38:42 -0500

    added student, course, and teacher classes

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,6 @@
+public class Course {
+        private String topic;
+        private Teacher instructor;
+        private ArrayList<Student> enrolledStudents;
+    }
+}
--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -4,6 +4,33 @@ public class Student {
         private int numberOfCredits = 0;
         private double gpa = 0.0;
 
+        public void setName(String name){
+                this.name = name;
+        }
+        public void setStudentId(int name){
+                this.studentId = studentId;
+        }
+        public void setNumberOfCredits(int numberOfCredits){
+                this.numberOfCredits = numberOfCredits;
+        }
+        public void setName(double gpa){
+                this.gpa = gpa;
+        }
+        public String getName(){
+                return name;
+        }
+        public int getStudentId(){
+                return studentId;
+        }
+        public int getNumberOfCredits(){
+
+                return numberOfCredits;
+        }
+        public double gpa(){
+                return gpa;
+        }
+
+
         // Drop your getters and setters below for the Student class.
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
