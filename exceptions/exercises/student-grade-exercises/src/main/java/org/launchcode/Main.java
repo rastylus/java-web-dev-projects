bec962379202e9820b73164c1f7b48a20de8807a
rastylus <ryan.p.riggs@gmail.com>
2023-09-25 14:51:19 -0500

    added upstream

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        System.out.println(Divide(6,6));
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,9 +14,10 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        System.out.println(studentFiles.values());
     }
 
-    public static int Divide(int x, int y)
+    public static String Divide(int x, int y)
     {
         if (y <= 0) {
             try {
@@ -24,11 +26,17 @@ public class Main {
                 e.printStackTrace();
             }
         }
-        return x/y;
+        return String.valueOf(x/y);
     }
 
-//    public static int CheckFileExtension(String fileName)
-//    {
-//        // Write code here!
-//    }
+    public static int CheckFileExtension(String fileName)
+    {
+        int points = 1;
+        if (!fileName.contains(".java")) {
+            points = 0;
+        } else {
+
+        }
+        return points;
+    }
 }
